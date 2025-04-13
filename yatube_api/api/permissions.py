8d8a23e04e9b284189848d8b0be64ef8a728197b
ksiim <makskrutchev@gmail.com>
2025-04-13 22:50:50 +0500

    commint

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,5 +1,6 @@
 from rest_framework import permissions
 
+
 class AuthorPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         # Require authentication for write actions (POST, PUT, PATCH, DELETE)
